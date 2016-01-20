1a3519d0a4712467aa8f4b314076a7e8662ebf28
David HAN SZE CHUEN <David.Han-Sze-Chuen@macquarie.com>
2016-01-20 17:14:17 -0600

    feat(package): fix import path

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var moduleVersion = '%version%'; // will be updated by a grunt task
 if (moduleVersion === '%' + 'version' + '%') { // useful for local testing before npm packaging
   require('./dist/ui-bootstrap-tpls-' + require('./package.json').version);
 } else {
-  require('./dist/ui-bootstrap-tpls-%version%'); // will be updated by a grunt task
+  // note: at that time, this file will run from the ./dist folder
+  // will be updated by a grunt task
+  require('./ui-bootstrap-tpls-%version%');
 }
 
 module.exports = 'ui.bootstrap';
