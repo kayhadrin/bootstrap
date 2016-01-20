8a45567f6ac4eef4ec47c4ea1558cf537bb8d619
David HAN SZE CHUEN <David.Han-Sze-Chuen@macquarie.com>
2016-01-20 17:40:00 -0600

    feat(package): prevent browserify from trying to load package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var moduleVersion = '%version%'; // will be updated by a grunt task
 
 if (moduleVersion === '%' + 'version' + '%') { // useful for local testing before npm packaging
-  require('./dist/ui-bootstrap-tpls-' + require('./package.json').version);
+  require('./dist/ui-bootstrap-tpls-' + require('./' + 'package.json').version);
 } else {
   // note: at that time, this file will run from the ./dist folder
   // will be updated by a grunt task
